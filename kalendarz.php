28a8e2d205a1459dd4b11f40ce35787faeaaee06
que2115 <70887703+que2115@users.noreply.github.com>
2021-10-20 17:16:43 +0200

    gotowe ssam robiłem w domu bez żadnej pomocy

---
+++ kalendarz.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style5.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mój kalendarz</title>
+</head>
+<body>
+    <div class="jeden">
+    <img src="loogo1.png" alt="MÓJ KALENDARZ">
+    </div>
+    <div class="dwa">
+        <h1>KALENDARZ</h1>
+        <?php
+
+        ?>
+    </div>
+    <main>
+        <?php
+
+        ?>
+    </main>
+    <footer>
+        <?php
+
+        ?>
+        <form method="POST">
+            DODAJ WPIS: <input type="text" name="zapis">
+            <button type="submit">DODAJ</button>
+        </form>
+        <p>STRONE WYKONAŁ: JAKUB TROKA :3</p>
+    </footer>
+</body>
+</html>
